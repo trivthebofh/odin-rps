8210b139bdf87c7c2d3055ba48e022c61150ee78
Travis Mehrer <travis@canerduh.com>
2023-07-24 15:56:18 -0600

    RPS Game Framework

--- js/rps.js
+++ js/rps.js
@@ -4,20 +4,45 @@
 
 // Computer will randomly generate one of Rock, Paper or Scissors
 
+// Listen for button click
+//button.addEventListener("click", updateButton);
+
 const rps = ["rock", "paper", "scissors"];
+const rock = 1, paper = 2, scissors = 3;
 
 function getComputerChoice(){
-    let compChoice = rps[(Math.floor(Math.random() * rps.length))];
-    console.log(compChoice);
-    return compChoice;
+    let computerSelection = rps[(Math.floor(Math.random() * rps.length))];
+    console.log(computerSelection);
+    return computerSelection;
 }
 
 function getUserChoice(){
-    
+    let userInputChoice = prompt('Choose \"rock\", \"paper\", or \"scissors\"').toLowerCase();
+    console.log(userInputChoice);
+    return userInputChoice
+}
+
+function determineWinner(playerSelection, computerSelection){
+    if ((playerSelection == "rock" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "paper") || (playerSelection == "scissors" && computerSelection == "scissors")) {
+        console.log ("It's a tie!");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("Paper covers Rock, Computer wins!");
+    }else if (playerSelection == "rock" && computerSelection == "scissors"){
+        console.log("Rock smashes Scissors, you win!");
+    }else if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("Paper covers Rock, you win!");
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("Scissors cuts Paper, Computer wins!");
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("Rock smashes Scissors, Computer wins!");
+    } else {
+        console.log("Scissors cut Paper, you win!");
+    }
 }
 
 let compMove = getComputerChoice();
 let userMove = getUserChoice();
+determineWinner(userMove, compMove);
 
 // The User will input their choice
 
